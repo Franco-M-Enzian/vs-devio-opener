04c192b86410f5bb60778dcefa75d658b1d72252
Franco-M-Enzian <emf0000b@gmail.com>
2024-10-20 15:16:36 +0900

    Start recording working time when vscode starts

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,33 @@
 import * as vscode from 'vscode';
 
+let startTime: Date;
+
 export function activate(context: vscode.ExtensionContext) {
+  "worktime-tracker.start";
+    startTime = new Date();
 
-  let disposable = vscode.commands.registerCommand(
-    'vs-devio-opener.helloWorld',
-    () => {
-      vscode.window.showInformationMessage('Hello World!');
-    }
-  );
-
-  let newDisposable = vscode.commands.registerCommand(
-    'vs-devio-opener.newCommand',
-    () => {
-      vscode.window.showInformationMessage('New Command!');
-    }
-  );
-
-
-  context.subscriptions.push(disposable);
-  context.subscriptions.push(newDisposable);
-
+    vscode.window.showInformationMessage('作業時間の記録を開始しました。');
+    const formattedTime = formatDate(startTime);
+    vscode.window.showInformationMessage('現在時刻：' + formattedTime);
 }
+
+function formatDate(date: Date): string {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  return `${year}年${month}月${day}日 ${hours}時${minutes}分${seconds}秒`;
+}
+
+// export function deactivate() {
+//   recordWorkTime();
+// }
+
+// function recordWorkTime() {
+//   const endTime = new Date();
+//   const workDuration = (endTime.getTime() - startTime.getTime()) / 1000;
+//   console.log(`作業時間: ${workDuration}秒`);
+// }
